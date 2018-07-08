96b0eb0ccdc6b4f2f81c423f79a4f0d7960ccf88
VoTaiTri <votaitri.hust@gmail.com>
2018-07-08 21:03:26 +0700

    update migration and model for HuobiPro

--- db/migrate/20180706101611_create_huobi_pro_transactions.rb
+++ db/migrate/20180706101611_create_huobi_pro_transactions.rb
@@ -1,13 +1,13 @@
 class CreateHuobiProTransactions < ActiveRecord::Migration[5.1]
   def change
     create_table :huobi_pro_transactions do |t|
-      t.references :huobi_pro_symbol, index: true, null: false
-      t.decimal :order_id, precision: 15, scale: 0, null: false
-      t.integer :type, index: true, limit: 1, default: 0
-      t.float :sell_price, limit: 15, null: false
-      t.float :sell_amount, limit: 20, null: false
+      t.references :huobi_pro_symbol, index: true 
+      t.decimal :order_id, precision: 15, scale: 0
+      t.integer :sell_type, index: true, limit: 1, default: 0
+      t.float :sell_price, limit: 15
+      t.float :sell_amount, limit: 20
       t.datetime :sell_time, null: false
-      t.float :profit, limit: 10, null: false
+      t.float :profit, limit: 10
       t.boolean :status, index: true, default: false
 
       t.timestamps null: false
