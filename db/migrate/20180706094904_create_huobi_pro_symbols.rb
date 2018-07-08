9da58ec0f6d37f0d4acde901688838a62950f0db
VoTaiTri <votaitri.hust@gmail.com>
2018-07-08 10:04:56 +0700

    updade migration file to add order_id field to Huobi tables

--- db/migrate/20180706094904_create_huobi_pro_symbols.rb
+++ db/migrate/20180706094904_create_huobi_pro_symbols.rb
@@ -1,15 +1,16 @@
 class CreateHuobiProSymbols < ActiveRecord::Migration[5.1]
   def change
     create_table :huobi_pro_symbols do |t|
+      t.decimal :order_id, precision: 15, scale: 0, null: false
       t.string :symbol, limit: 15, index: true, null: false
-      t.float :buy_price, null: false
-      t.float :buy_amount, null: false
+      t.float :buy_price, limit: 15, null: false
+      t.float :buy_amount, limit: 20, null: false
       t.datetime :buy_time, null: false
-      t.float :target1
-      t.float :target2
-      t.float :target3
-      t.float :stoploss
-      t.float :profit
+      t.float :target1, limit: 15
+      t.float :target2, limit: 15
+      t.float :target3, litmit: 15
+      t.float :stoploss, limit: 15
+      t.float :profit, limit: 10
       t.boolean :status, index: true, default: false
 
       t.timestamps null: false
